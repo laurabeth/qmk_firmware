8b017c0a536567ffe915ab56416c3a9f7c3f49f9
Laura Bhayani <14044896+laurabeth@users.noreply.github.com>
2021-01-01 18:13:31 -0500

    tweak elise layout

--- keyboards/primekb/prime_e/keymaps/laurabeth/keymap.c
+++ keyboards/primekb/prime_e/keymaps/laurabeth/keymap.c
@@ -29,24 +29,24 @@ enum elise_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-		KC_TAB,   KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,          KC_Y,     KC_U,     KC_I, KC_O,    KC_P,    KC_BSPC, KC_DEL,
-		ESC_NAV,  KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,     KC_J,     KC_K, KC_L,    KC_SCLN, KC_QUOT,
+		ESC_NAV,   KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,         KC_Y,     KC_U,     KC_I, KC_O,    KC_P,    KC_DEL, KC_BSPC,
+		ESC_TAB,  KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,     KC_J,     KC_K, KC_L,    KC_SCLN, KC_QUOT,
 		KC_LSFT,  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,          KC_B,     KC_N,     KC_M, KC_COMM, KC_DOT,  SFT_SLS, SFT_SLS,
-		KC_LCTL,  KC_LALT,                    KC_NO,   ENT_LOW,	      SPC_UPR,  KC_RALT,                          KC_RGUI, KC_RCTL
+		KC_LCTL,  KC_LALT,                    KC_NO,   ENT_LOW,	      SPC_UPR,  KC_RGUI,                          KC_RALT, KC_RCTL
     ),
 
     [_GAMING] = LAYOUT(
-		KC_TAB,   KC_Q,     KC_W,     KC_E,   KC_R,       KC_T,          KC_Y,    KC_U,     KC_I, KC_O,    KC_P,    KC_BSPC, KC_DEL,
-		KC_ESC,   KC_A,     KC_S,     KC_D,   KC_F,       KC_G,          KC_H,    KC_J,     KC_K, KC_L,    KC_SCLN, KC_ENT,
+		KC_ESC,   KC_Q,     KC_W,     KC_E,   KC_R,       KC_T,          KC_Y,    KC_U,     KC_I, KC_O,    KC_P,    KC_DEL, KC_BSPC,
+		KC_TAB,   KC_A,     KC_S,     KC_D,   KC_F,       KC_G,          KC_H,    KC_J,     KC_K, KC_L,    KC_SCLN, KC_ENT,
 		KC_LSFT,  KC_Z,     KC_X,     KC_C,   KC_V,       KC_B,          KC_NO,   KC_N,     KC_M, KC_COMM, KC_DOT,  KC_SLSH, MO(_NAVIGATE),
-		KC_LCTL,  KC_LALT,                    MO(_RAISE), KC_SPC,	     KC_ENT,  KC_RALT,                          KC_RGUI, KC_RCTL
+		KC_LCTL,  KC_LALT,                    MO(_RAISE), KC_SPC,	     KC_ENT,  KC_RGUI,                          KC_RALT, KC_RCTL
     ),
 
     [_LOWER] = LAYOUT(
 		KC_GRV,    KC_EXLM,   KC_AT,     KC_HASH,   KC_DLR,     KC_PERC,          KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_LPRN,   KC_RPRN,   KC_VOLD,   KC_VOLU,
 		KC_TRNS,   KC_F1,     KC_F2,     KC_F3,     KC_F4,      KC_F5,            KC_F6,     KC_UNDS,   KC_PLUS,   KC_LCBR,   KC_RCBR,   KC_PIPE,
 		KC_CAPS,   KC_F7,     KC_F8,     KC_F9,     KC_F10,     KC_F11,           KC_HOME,   KC_F12,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-		BL_TOGG,   BL_STEP,                         KC_TRNS,    KC_TRNS,	      KC_TRNS,   KC_TRNS,                                    KC_TRNS,   KC_TRNS
+		BL_TOGG,   BL_STEP,                         KC_TRNS,    KC_TRNS,	        KC_TRNS,   KC_TRNS,                                    KC_TRNS,   KC_TRNS
     ),
 
     [_RAISE] = LAYOUT(
