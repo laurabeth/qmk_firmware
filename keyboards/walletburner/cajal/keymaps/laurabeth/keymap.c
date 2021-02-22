876035418d192b436d9f3134ac14178db013b4dc
Laura Bhayani <14044896+laurabeth@users.noreply.github.com>
2021-02-22 09:22:36 -0500

    tweak cajal layout

--- keyboards/walletburner/cajal/keymaps/laurabeth/keymap.c
+++ keyboards/walletburner/cajal/keymaps/laurabeth/keymap.c
@@ -29,6 +29,7 @@ enum cajal_keycodes {
 
 // Layer switch
 #define ADJUST MO(_ADJUST)
+#define LOWER MO(_LOWER)
 #define NAV MO(_NAVIGATE)
 #define RAISE MO(_RAISE)
 
@@ -49,19 +50,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,  KC_E,   KC_R,   KC_T, KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_DEL, KC_BSPC, KC_TRNS,
         KC_TAB,  KC_A,    KC_S,  KC_D,   KC_F,   KC_G, KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,  KC_C,   KC_V,   KC_B, KC_N,   KC_M,    KC_COMM, KC_DOT, KC_LSFT, KC_UP,
-        KC_LCTL, KC_LALT, RAISE, KC_SPC, KC_SPC, NAV,  ADJUST, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LALT, RAISE, KC_SPC, KC_SPC, NAV,  LOWER,  KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_LOWER] = LAYOUT_stagger(
         KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_PLUS, KC_MINS, KC_EQL,  KC_UNDS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_PIPE,
-        KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP,
+        KC_TRNS, KC_PLUS, KC_EQL,  KC_MINS, KC_UNDS, KC_PIPE, KC_TRNS, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_BSLS,
+        KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END
     ),
 
     [_RAISE] = LAYOUT_stagger(
         KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
@@ -83,15 +84,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-    case 1:
+    case _QWERTY:
         writePinHigh(B7);
         writePinLow(B6);
         break;
-    case 2:
+    case _GAMING:
         writePinLow(B7);
         writePinHigh(B6);
         break;
-    case 3:
+    case _NAVIGATE:
         writePinHigh(B7);
         writePinHigh(B6);
         break;
