e83e9d4735b97e5090950d3db63efece0de49be7
Laura Bhayani <14044896+laurabeth@users.noreply.github.com>
2021-03-05 10:31:36 -0500

    tweak the40

--- keyboards/lazydesigners/the40/keymaps/laurabeth/keymap.c
+++ keyboards/lazydesigners/the40/keymaps/laurabeth/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         TAB_NAV,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,
         KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  SFT_SLS,
-        KC_LCTL,   KC_TRNS, KC_LALT,          ENT_LOW,          SPC_UPR,          KC_RGUI, KC_TRNS,   KC_RCTL
+        KC_LCTL,   KC_TRNS, KC_LALT,          ENT_LOW,          SPC_UPR,          KC_RGUI, KC_TRNS, KC_RCTL
         ),
     [_GAMING] = LAYOUT_all(
         KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
@@ -49,15 +49,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
     [_LOWER] = LAYOUT_all(
         KC_GRV,    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-        KC_TRNS,   KC_PLUS, KC_MINS, KC_EQL,  KC_UNDS, KC_PIPE, KC_SCLN, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR,
-        KC_CAPS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_TRNS, KC_SCLN, KC_TRNS,
+        KC_TRNS,   KC_PLUS, KC_EQL,  KC_MINS, KC_UNDS, KC_PIPE, KC_COLN, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR,
+        KC_CAPS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SCLN, KC_BSLS,
         KC_TRNS,   KC_TRNS, KC_NO,            KC_NO,            KC_NO,            KC_NO,   KC_TRNS, KC_TRNS
         ),
     [_RAISE] = LAYOUT_all(
         KC_TRNS,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-        KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, KC_COLN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_CAPS,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,     KC_TRNS, KC_NO,        KC_NO,          KC_NO,          KC_NO, KC_TRNS, KC_TRNS
+        KC_TRNS,     KC_TRNS, KC_NO,            KC_NO,            KC_NO,            KC_NO,   KC_TRNS, KC_TRNS
         ),
     [_NAVIGATE] = LAYOUT_all(
         KC_TRNS,     T_QWRT,  KC_TRNS,   KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS,  KC_TRNS, KC_TRNS,
